ca7ccbceb94dd0f534eeaab4a1f0380694e6306e
wzrddd <d.kalynychenko@gmail.com>
2023-11-12 22:30:18 +0200

    Render impl

--- ChessConsole/Pieces/Pawn.cs
+++ ChessConsole/Pieces/Pawn.cs
@@ -5,7 +5,12 @@ namespace ChessConsole.Pieces;
 public class Pawn : Piece
 {
     public Pawn(Color color, Coordinates coordinates) : base(color, coordinates) {}
-    
+
+    public override string ToString()
+    {
+        return Color == Color.White ? "P" : "p";
+    }
+
     public override void Move(Coordinates coordinates)
     {
         throw new NotImplementedException();
