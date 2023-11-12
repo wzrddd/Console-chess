fa742b06f6e0a63faf7910bac439d49a98ebe128
ILARx <xilarx76@gmail.com>
2023-11-12 22:30:18 +0200

    Render impl

--- ChessConsole/Game.cs
+++ ChessConsole/Game.cs
@@ -8,11 +8,24 @@ public class Game
     public Dictionary<Coordinates, Piece> Pieces = new()
     {
         {new Coordinates(0, Rank.A), new Pawn(Color.White, new Coordinates(0, Rank.A))},
-        {new Coordinates(9, Rank.H), new Pawn(Color.Black, new Coordinates(9, Rank.H))}
+        {new Coordinates(7, Rank.H), new Pawn(Color.Black, new Coordinates(9, Rank.H))}
     };
 
     public void Render()
     {
-        Console.WriteLine("\u2655");
+        Console.WriteLine("   (#1)(#2)(#3)(#4)(#5)(#6)(#7)(#8)");
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (j == 0) Console.Write("(" + (Rank)i + ")");
+
+                if (Pieces.ContainsKey(new Coordinates(j, (Rank)i)))
+                    Console.Write($"[{Pieces[new Coordinates(j, (Rank)i)]}]");
+                else if(!Pieces.ContainsKey(new Coordinates(j, (Rank)i)))
+                    Console.Write("[ ]");
+            }
+            Console.WriteLine();
+        }
     }
 }
