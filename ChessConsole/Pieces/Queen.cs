8d9eadf5ac897d8885fc471940036dfe01c0373e
wzrddd <d.kalynychenko@gmail.com>
2024-02-05 17:17:25 +0200

    queen move validation

--- ChessConsole/Pieces/Queen.cs
+++ ChessConsole/Pieces/Queen.cs
@@ -16,6 +16,50 @@ public class Queen(Color color, Coordinates coordinates) : IPiece
     {
         var xDiff = Math.Abs((int)cordFrom.Rank - (int)cordTo.Rank);
         var yDiff = Math.Abs(cordFrom.File - cordTo.File);
-        return xDiff == yDiff ||  xDiff <= 1 && yDiff <= 1 || cordFrom.Rank == cordTo.Rank || cordFrom.File == cordTo.File;
+
+        if (xDiff == yDiff)
+        {
+            var yMin = Math.Min(cordFrom.File, cordTo.File);
+            var xMin = Math.Min((int)cordFrom.Rank, (int)cordTo.Rank);
+            var xMax = Math.Max((int)cordFrom.Rank, (int)cordTo.Rank);
+            
+            var j = yMin + 1;
+            for (int i = xMin + 1; i < xMax; i++)
+            {
+                if (Game.Pieces.ContainsKey(new Coordinates(j, (Rank)i)))
+                {
+                    return false;
+                }
+                j++;
+            }
+        }        
+        
+        if (cordFrom.Rank == cordTo.Rank)
+        {
+            var min = Math.Min(cordFrom.File, cordTo.File);
+            var max = Math.Max(cordFrom.File, cordTo.File);
+            for (var i = min + 1; i < max; i++)
+            {
+                if (Game.Pieces.ContainsKey(new Coordinates(i, cordFrom.Rank)))
+                {
+                    return false;
+                }
+            }
+        }
+        
+        if (cordFrom.File == cordTo.File)
+        {
+            var min = Math.Min((int)cordFrom.Rank, (int)cordTo.Rank);
+            var max = Math.Max((int)cordFrom.Rank, (int)cordTo.Rank);
+            for (var i = min + 1; i < max; i++)
+            {
+                if (Game.Pieces.ContainsKey(new Coordinates(cordFrom.File, (Rank)i)))
+                {
+                    return false;
+                }
+            }
+        }
+        
+        return xDiff == yDiff || cordFrom.Rank == cordTo.Rank || cordFrom.File == cordTo.File;
     }
 }
