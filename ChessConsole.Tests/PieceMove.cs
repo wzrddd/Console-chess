7fbba2eac1ca3cb3ee535dc5d3f18652d4aa7433
wzrddd <d.kalynychenko@gmail.com>
2024-02-16 22:56:06 +0200

    migrate from MSTest to NUnit

---
+++ ChessConsole.Tests/PieceMove.cs
@@ -0,0 +1,131 @@
+using ChessConsole.Enums;
+using ChessConsole.Pieces;
+
+namespace ChessConsole.Tests;
+
+public class PieceMove
+{
+    private readonly Dictionary<Coordinates, IPiece> _testPosition = new ()
+    {
+        {new Coordinates(0, Rank.A), new King(Color.White, new Coordinates(0, Rank.A))},
+        {new Coordinates(0, Rank.B), new Queen(Color.White, new Coordinates(0, Rank.B))},
+        {new Coordinates(0, Rank.C), new Rook(Color.White, new Coordinates(0, Rank.C))},
+        {new Coordinates(0, Rank.D), new Bishop(Color.White, new Coordinates(0, Rank.D))},
+        {new Coordinates(0, Rank.E), new Knight(Color.White, new Coordinates(0, Rank.E))},
+        {new Coordinates(0, Rank.F), new Pawn(Color.White, new Coordinates(0, Rank.F))},
+            
+            
+        {new Coordinates(7, Rank.A), new King(Color.Black, new Coordinates(0, Rank.A))},
+        {new Coordinates(7, Rank.B), new Queen(Color.Black, new Coordinates(0, Rank.B))},
+        {new Coordinates(7, Rank.C), new Rook(Color.Black, new Coordinates(0, Rank.C))},
+        {new Coordinates(7, Rank.D), new Bishop(Color.Black, new Coordinates(0, Rank.D))},
+        {new Coordinates(7, Rank.E), new Knight(Color.Black, new Coordinates(0, Rank.E))},
+        {new Coordinates(7, Rank.F), new Pawn(Color.Black, new Coordinates(0, Rank.F))},
+    };
+    
+    public class CoordinatesTestCase(Coordinates cordFrom, Coordinates cordTo, bool result, string testName)
+    {
+        public string TestName { get; private set; } = testName;
+        public Coordinates CordFrom { get; set; } = cordFrom;
+        public Coordinates CordTo { get; set; } = cordTo;
+        public bool Result { get; set; } = result;
+
+        public override string ToString()
+        {
+            return TestName;
+        }
+    }
+    
+    private static IEnumerable<CoordinatesTestCase> CoordinatesTestCases
+    {
+        get
+        {
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.F),
+                new Coordinates(1, Rank.F), 
+                true, 
+                "TestValidPawnMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.F),
+                new Coordinates(4, Rank.F), 
+                false,
+                "TestInvalidPawnMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.E),
+                new Coordinates(2, Rank.F), 
+                true,
+                "TestValidKnightMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.E),
+                new Coordinates(4, Rank.D), 
+                false,
+                "TestInvalidKnightMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.D),
+                new Coordinates(4, Rank.H), 
+                true,
+                "TestValidBishopMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.D),
+                new Coordinates(3, Rank.H), 
+                false,
+                "TestInvalidBishopMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.C),
+                new Coordinates(5, Rank.C), 
+                true,
+                "TestValidRookMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.C),
+                new Coordinates(6, Rank.B), 
+                false,
+                "TestInvalidRookMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.B),
+                new Coordinates(3, Rank.E), 
+                true,
+                "TestValidQueenMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.B),
+                new Coordinates(5, Rank.D), 
+                false,
+                "TestInvalidQueenMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.A),
+                new Coordinates(1, Rank.A), 
+                true,
+                "TestValidKingMove");
+            
+            yield return new CoordinatesTestCase(
+                new Coordinates(0, Rank.A),
+                new Coordinates(3, Rank.A),
+                false,
+                "TestInvalidKingMove");
+        }
+    }
+    
+    [SetUp]
+    public void Setup()
+    {
+        Game.Pieces = _testPosition;
+    }
+
+    [Test, TestCaseSource(nameof(CoordinatesTestCases))]
+    public void TestMove(CoordinatesTestCase coordinatesTestCase)
+    {
+        var piece = Game.Pieces[coordinatesTestCase.CordFrom];
+        var isMoveValid = piece.IsMoveValid(coordinatesTestCase.CordFrom, coordinatesTestCase.CordTo);
+        
+        Assert.That(isMoveValid, Is.EqualTo(coordinatesTestCase.Result));
+    }
+}
