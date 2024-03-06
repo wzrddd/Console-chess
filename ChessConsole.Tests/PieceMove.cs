c71ed658a9539fc087b54b2c171e9ff8f8fc648b
ILARx <xilarx76@gmail.com>
2024-03-06 11:27:44 +0200

    PieceTake setup + refactor

--- ChessConsole.Tests/PieceMove.cs
+++ ChessConsole.Tests/PieceMove.cs
@@ -30,135 +30,154 @@ public class PieceMove
         {new Coordinates(6, Rank.F), new Pawn(Color.Black, new Coordinates(0, Rank.F))},
     };
     
-    public class CoordinatesTestCase(Coordinates cordFrom, Coordinates cordTo, bool result, string testName)
-    {
-        public string TestName { get; private set; } = testName;
-        public Coordinates CordFrom { get; set; } = cordFrom;
-        public Coordinates CordTo { get; set; } = cordTo;
-        public bool Result { get; set; } = result;
-
-        public override string ToString()
-        {
-            return TestName;
-        }
-    }
-    
-    private static IEnumerable<CoordinatesTestCase> CoordinatesTestCases
+    private static IEnumerable<PieceTestCase> PieceValidMoveTestCases
     {
         get
         {
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.F),
                 new Coordinates(1, Rank.F), 
                 true, 
                 "TestValidPawnMove");
             
-            yield return new CoordinatesTestCase(
-                new Coordinates(0, Rank.F),
-                new Coordinates(4, Rank.F), 
-                false,
-                "TestInvalidPawnMove");
-
-            yield return new CoordinatesTestCase(
-                new Coordinates(7, Rank.F),
-                new Coordinates(5, Rank.F),
-                false,
-                "TestBlockPawnMove");
-            
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.E),
                 new Coordinates(2, Rank.F), 
                 true,
                 "TestValidKnightMove");
             
-            yield return new CoordinatesTestCase(
-                new Coordinates(0, Rank.E),
-                new Coordinates(4, Rank.D), 
-                false,
-                "TestInvalidKnightMove");
-            
-            yield return new CoordinatesTestCase(
-                new Coordinates(7, Rank.E),
-                new Coordinates(5, Rank.F),
-                true,
-                "TestBlockKnightMove");
-            
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.D),
                 new Coordinates(4, Rank.H), 
                 true,
                 "TestValidBishopMove");
             
-            yield return new CoordinatesTestCase(
-                new Coordinates(0, Rank.D),
-                new Coordinates(3, Rank.H), 
-                false,
-                "TestInvalidBishopMove");
-            
-            yield return new CoordinatesTestCase(
-                new Coordinates(7, Rank.D),
-                new Coordinates(5, Rank.B),
-                false,
-                "TestBlockBishopMove");
-            
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.C),
                 new Coordinates(5, Rank.C), 
                 true,
                 "TestValidRookMove");
             
-            yield return new CoordinatesTestCase(
-                new Coordinates(0, Rank.C),
-                new Coordinates(5, Rank.B), 
-                false,
-                "TestInvalidRookMove");
-            
-            yield return new CoordinatesTestCase(
-                new Coordinates(7, Rank.C),
-                new Coordinates(5, Rank.C),
-                false,
-                "TestBlockRookMove");
-            
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.B),
                 new Coordinates(3, Rank.E), 
                 true,
                 "TestValidQueenMove");
             
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
+                new Coordinates(0, Rank.A),
+                new Coordinates(1, Rank.A), 
+                true,
+                "TestValidKingMove");
+        }
+    }
+
+    private static IEnumerable<PieceTestCase> PieceInvalidMoveTestCases
+    {
+        get
+        {
+            yield return new PieceTestCase(
+                new Coordinates(0, Rank.F),
+                new Coordinates(4, Rank.F), 
+                false,
+                "TestInvalidPawnMove");
+            
+            yield return new PieceTestCase(
+                new Coordinates(0, Rank.E),
+                new Coordinates(4, Rank.D), 
+                false,
+                "TestInvalidKnightMove");
+            
+            yield return new PieceTestCase(
+                new Coordinates(0, Rank.D),
+                new Coordinates(3, Rank.H), 
+                false,
+                "TestInvalidBishopMove");
+            
+            yield return new PieceTestCase(
+                new Coordinates(0, Rank.C),
+                new Coordinates(5, Rank.B), 
+                false,
+                "TestInvalidRookMove");
+            
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.B),
                 new Coordinates(5, Rank.D), 
                 false,
                 "TestInvalidQueenMove");
             
-            yield return new CoordinatesTestCase(
-                new Coordinates(7, Rank.B),
-                new Coordinates(5, Rank.B),
-                false,
-                "TestBlockQueenMove");
-            
-            yield return new CoordinatesTestCase(
-                new Coordinates(0, Rank.A),
-                new Coordinates(1, Rank.A), 
-                true,
-                "TestValidKingMove");
-            
-            yield return new CoordinatesTestCase(
+            yield return new PieceTestCase(
                 new Coordinates(0, Rank.A),
                 new Coordinates(3, Rank.A),
                 false,
                 "TestInvalidKingMove");
         }
     }
-    
+
+    private static IEnumerable<PieceTestCase> PieceBlockedMoveTestCases
+    {
+        get
+        {
+            yield return new PieceTestCase(
+                new Coordinates(7, Rank.F),
+                new Coordinates(5, Rank.F),
+                false,
+                "TestBlockPawnMove");
+            
+            
+            yield return new PieceTestCase(
+                new Coordinates(7, Rank.E),
+                new Coordinates(5, Rank.F),
+                true,
+                "TestBlockKnightMove");
+            
+            
+            yield return new PieceTestCase(
+                new Coordinates(7, Rank.D),
+                new Coordinates(5, Rank.B),
+                false,
+                "TestBlockBishopMove");
+            
+            yield return new PieceTestCase(
+                new Coordinates(7, Rank.C),
+                new Coordinates(5, Rank.C),
+                false,
+                "TestBlockRookMove");
+            
+            yield return new PieceTestCase(
+                new Coordinates(7, Rank.B),
+                new Coordinates(5, Rank.B),
+                false,
+                "TestBlockQueenMove");
+        }
+    }
+
     [SetUp]
     public void Setup()
     {
         Game.Pieces = _testPosition;
     }
 
-    [Test, TestCaseSource(nameof(CoordinatesTestCases))]
-    public void TestMove(CoordinatesTestCase coordinatesTestCase)
+    [Test, TestCaseSource(nameof(PieceValidMoveTestCases))]
+    public void TestValidMove(PieceTestCase coordinatesTestCase)
+    {
+        var piece = Game.Pieces[coordinatesTestCase.CordFrom];
+        var isMoveValid = piece.IsMoveValid(coordinatesTestCase.CordFrom, coordinatesTestCase.CordTo);
+        
+        Assert.That(isMoveValid, Is.EqualTo(coordinatesTestCase.Result));
+    }
+    
+    [Test, TestCaseSource(nameof(PieceInvalidMoveTestCases))]
+    public void TestInvalidMove(PieceTestCase coordinatesTestCase)
+    {
+        var piece = Game.Pieces[coordinatesTestCase.CordFrom];
+        var isMoveValid = piece.IsMoveValid(coordinatesTestCase.CordFrom, coordinatesTestCase.CordTo);
+        
+        Assert.That(isMoveValid, Is.EqualTo(coordinatesTestCase.Result));
+    }
+    
+    [Test, TestCaseSource(nameof(PieceBlockedMoveTestCases))]
+    public void TestBlockedMove(PieceTestCase coordinatesTestCase)
     {
         var piece = Game.Pieces[coordinatesTestCase.CordFrom];
         var isMoveValid = piece.IsMoveValid(coordinatesTestCase.CordFrom, coordinatesTestCase.CordTo);
