d98c85315f93766c326c2dedc52675f47a3f23ae
wzrddd <d.kalynychenko@gmail.com>
2024-03-15 21:27:35 +0200

    castling impl

--- ChessConsole/Pieces/King.cs
+++ ChessConsole/Pieces/King.cs
@@ -4,6 +4,8 @@ namespace ChessConsole.Pieces;
 
 public class King(Color color) : Piece(color)
 {
+    public bool IsFirstMove = true;
+
     public override string ToString()
     {
         return Color == Color.White ? "K" : "k";
@@ -15,12 +17,38 @@ public class King(Color color) : Piece(color)
         var yDiff = Math.Abs(cordFrom.File - cordTo.File);
         var isMoveCorrect = xDiff <= 1 && yDiff <= 1;
 
+        if (Game.Pieces[cordTo] is Rook rook && IsFirstMove && rook.Color == color && rook.IsFirstMove)
+        {
+            Castle(cordFrom, cordTo);
+            return true;
+        }
+        
         if (Game.Pieces.ContainsKey(cordTo) && isMoveCorrect)
         {
             return TryTake(cordFrom, cordTo);
         }
         
         return isMoveCorrect;
+    }
+
+    private void Castle(Coordinates cordFrom, Coordinates cordTo)
+    {
+        var pieceCordFrom = Game.Pieces[cordFrom];
+        var pieceCordTo = Game.Pieces[cordTo];
+        
+        Game.Pieces.Remove(cordTo);
+        Game.Pieces.Remove(cordFrom);
+
+        if (cordTo.Rank == Rank.A)
+        {
+            Game.Pieces.Add(new Coordinates(cordFrom.File, Rank.C), pieceCordFrom);
+            Game.Pieces.Add(new Coordinates(cordTo.File, Rank.D), pieceCordTo);
+        }
+        else
+        {
+            Game.Pieces.Add(new Coordinates(cordFrom.File, Rank.G), pieceCordFrom);
+            Game.Pieces.Add(new Coordinates(cordTo.File, Rank.F), pieceCordTo);
+        }
     }
 
     protected override void RemovePiece(Coordinates coordinates)
